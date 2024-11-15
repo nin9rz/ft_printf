b241c07568f7e208191e564e37da8d42c78a80ec
Jean Nibaudeau <jenibaud@2G7.42angouleme.fr>
2024-11-15 15:18:44 +0100

    adding some utils

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,24 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jenibaud <jenibaud@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 15:05:35 by jenibaud          #+#    #+#             */
-/*   Updated: 2024/11/15 15:17:24 by jenibaud         ###   ########.fr       */
+/*   Created: 2024/11/15 15:13:17 by jenibaud          #+#    #+#             */
+/*   Updated: 2024/11/15 15:16:56 by jenibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# include <unistd.h>
-# include <stdarg.h>
-# include <stdlib.h>
+#include "ft_printf.h"
 
-int		ft_printf(const char *, ...);
-void	ft_putchar(char c);
-void	ft_putstr(char *s);
-size_t	ft_strlen(const char *s);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
-#endif
+void	ft_putstr(char *s)
+{
+	if (!s)
+		return ;
+	write(1, s, ft_strlen(s));
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
