fd1e29a80ba1193c925dc149bb529f18890579f1
Jean Nibaudeau <jenibaud@2G7.42angouleme.fr>
2024-11-15 15:11:36 +0100

    adding .h file

---
+++ ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jenibaud <jenibaud@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 15:05:35 by jenibaud          #+#    #+#             */
+/*   Updated: 2024/11/15 15:10:23 by jenibaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
